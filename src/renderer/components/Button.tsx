db039f5442c0ad88d9b08d0672fd535cf662e3d5
SimantoR <md.simanto_rahman@yahoo.com>
2020-02-04 03:00:26 -0330

    Added Button component with sound

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 
 interface Props extends React.HTMLAttributes<HTMLButtonElement> {
-  audioSrc?: string
+  audioSrc?: string;
+  animated: boolean;
 }
 
 interface State {
-  audio?: HTMLAudioElement
+  audio?: HTMLAudioElement;
 }
 
 class Button extends Component<Props, State> {
+  static defaultProps: Partial<Props> = {
+    animated: false
+  };
+
   constructor(props: Props) {
     super(props);
     this.state = {
-      audio: this.props.audioSrc ? new Audio(this.props.audioSrc) : undefined
-    }
+      audio: this.props.audioSrc
+        ? new Audio(this.props.audioSrc)
+        : new Audio(require('../resources/btn_audio.wav'))
+    };
   }
 
   onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const { onClick } = this.props;
     const { audio } = this.state;
-    if (onClick)
-      onClick(e);
-    if (audio)
-      audio.play();
-  }
+    if (audio) audio.play();
+    if (onClick) onClick(e);
+  };
 
   render() {
+    const {
+      children,
+      audioSrc,
+      animated,
+      className,
+      ...buttonProps
+    } = this.props;
+
+    let _className = className ? `${className} ` : '';
+    if (animated) 
+      _className += 'animated';
+
     return (
-      <button {...this.props} onClick={this.onClick}>
-        {this.props.children}
+      <button {...buttonProps} className={_className} onClick={this.onClick}>
+        <span style={{ zIndex: 0 }}>
+          {children}
+        </span>
       </button>
     );
   }
